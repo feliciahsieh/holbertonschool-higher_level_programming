2de33788b91a49f1b6e6ea6deeabe708a580316c
feliciahsieh <felicia@tixwriteoff.com>
2017-08-01 21:59:11 +0000

    removed newline

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-for y in range(0, 10):
+for y in range(0, 9):
     for x in range(y+1, 10):
         if (y == 8 and x == 9):
             print ('{}'.format(y), '{}'.format(x), sep="")
         else:
             print ('{}'.format(y), '{}'.format(x), ", ", sep="", end="")
-print("\n")
