4b71d7e03c39f68fa31208870125717022f98bb8
feliciahsieh <felicia@tixwriteoff.com>
2017-08-01 21:55:57 +0000

    pep8 styling

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,8 +2,7 @@
 for y in range(0, 10):
     for x in range(y+1, 10):
         if (y == 8 and x == 9):
-             print ('{}'.format(y), '{}'.format(x), sep="")
-#            print ('{}'.format(y), '{}'.format(x), sep="", end="")
+            print ('{}'.format(y), '{}'.format(x), sep="")
         else:
             print ('{}'.format(y), '{}'.format(x), ", ", sep="", end="")
 print("\n")
