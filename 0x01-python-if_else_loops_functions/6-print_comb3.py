5862b82e2650407827122e35d8ed903c0ba8cac7
feliciahsieh <felicia@tixwriteoff.com>
2017-08-01 22:07:55 +0000

    betty

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for y in range(0, 9):
     for x in range(y + 1, 10):
         if (y != 8 or x != 9):
-            print ('{}'.format(y), '{}'.format(x), ", ", sep="", end="")
+            print ('{:d}{:d}'.format(y, x), ", ", sep="", end="")
         else:
-            print ('{}'.format(y), '{}'.format(x), sep="")
+            print ('{:d}'.format(y), '{}'.format(x), sep="")
