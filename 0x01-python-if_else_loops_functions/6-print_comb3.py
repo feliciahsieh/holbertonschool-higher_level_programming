b232eaa8ddfd7d46dd5d919fa45eb891f15663f8
feliciahsieh <felicia@tixwriteoff.com>
2017-08-01 22:03:46 +0000

    reversed if/else statements

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for y in range(0, 9):
-    for x in range(y+1, 10):
-        if (y == 8 and x == 9):
-            print ('{}'.format(y), '{}'.format(x), sep="")
-        else:
+    for x in range(y + 1, 10):
+        if (y != 8 or x != 9):
             print ('{}'.format(y), '{}'.format(x), ", ", sep="", end="")
+        else:
+            print ('{}'.format(y), '{}'.format(x), sep="")
