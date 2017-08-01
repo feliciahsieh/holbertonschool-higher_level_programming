6ebe51811c0390691247d1af1985bfa65f42d82b
feliciahsieh <felicia@tixwriteoff.com>
2017-08-01 22:18:18 +0000

    remove parenthesis

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for y in range(0, 9):
     for x in range(y + 1, 10):
-        if (y != 8 or x != 9):
-            print ('{:d}{:d}'.format(y, x), end=", ")
+        if y != 8 or x != 9:
+            print('{:d}{:d}'.format(y, x), end=", ")
         else:
-            print ('{:d}{:d}'.format(y, x))
+            print('{:d}{:d}'.format(y, x))
