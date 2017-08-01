0709ae5c83afe3fcd7d75768f7e0118d5f58d97b
feliciahsieh <felicia@tixwriteoff.com>
2017-08-01 21:54:13 +0000

    removed end='' from last iteration

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,8 @@
 for y in range(0, 10):
     for x in range(y+1, 10):
         if (y == 8 and x == 9):
-            print ('{}'.format(y), '{}'.format(x), sep="", end="")
+             print ('{}'.format(y), '{}'.format(x), sep="")
+#            print ('{}'.format(y), '{}'.format(x), sep="", end="")
         else:
             print ('{}'.format(y), '{}'.format(x), ", ", sep="", end="")
 print("\n")
