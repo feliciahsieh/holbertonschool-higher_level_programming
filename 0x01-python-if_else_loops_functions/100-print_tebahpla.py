911a351d186a76b7f72fc0859d0898b253fd0259
feliciahsieh <felicia@tixwriteoff.com>
2017-08-02 01:58:49 +0000

    first commit

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+offset = 0
+for i in range(25, -1, -1):
+    if i % 2 == 0:
+        offset = ord('A') - ord('a')
+    else:
+        offset = 0
+    print(chr(i + ord('a') + offset), end="")
