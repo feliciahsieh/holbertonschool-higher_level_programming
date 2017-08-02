8d9bb2220e7c421bde2681eb58dec7894e897bc1
feliciahsieh <felicia@tixwriteoff.com>
2017-08-02 02:45:57 +0000

    remove test statements

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -7,9 +7,3 @@ def remove_char_at(str, n):
         return(result)
     else:
         return(str)
-
-print(remove_char_at("Holberton School", 3))
-print(remove_char_at("Chicago", 2))
-print(remove_char_at("C is fun!", 0))
-print(remove_char_at("School", 10))
-print(remove_char_at("Python", -2))
