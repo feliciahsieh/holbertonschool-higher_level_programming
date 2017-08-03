e0a175ae954eaaca78e5a62e9140b4558f395a34
feliciahsieh <felicia@tixwriteoff.com>
2017-08-03 22:18:52 +0000

    removed test code

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import re
     import hidden_4
-    regex = r"__"
     for i in dir(hidden_4):
         if i[:2] != "__":
             print('{:s}'.format(i))
