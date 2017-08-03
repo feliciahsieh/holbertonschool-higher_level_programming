89025387d279102644c790d69890af1f81122130
feliciahsieh <felicia@tixwriteoff.com>
2017-08-03 21:06:22 +0000

    first commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    count = len(sys.argv)
+    if count == 1:
+        print("0 argument.")
+        sys.exit(0)
+    elif count == 2:
+        print("1 argument:")
+
+    print('{:d} arguments'.format(count - 1))
+
+    for i in range(1, count):
+        print('{:d}: {:s}'.format(i, sys.argv[i]))
