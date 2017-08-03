232dbfb26ac6a3249a02b1f2bba98c374b095dd0
feliciahsieh <felicia@tixwriteoff.com>
2017-08-03 21:10:56 +0000

    indent problem with else statement

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
         sys.exit(0)
     elif count == 2:
         print("1 argument:")
-
-    print('{:d} arguments'.format(count - 1))
+    else:
+        print('{:d} arguments'.format(count - 1))
 
     for i in range(1, count):
         print('{:d}: {:s}'.format(i, sys.argv[i]))
