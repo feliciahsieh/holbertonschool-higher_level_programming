ce70808fcc727fbf17f270b823d475005d955f0e
feliciahsieh <felicia@tixwriteoff.com>
2017-08-03 21:27:21 +0000

    first commit

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    count = len(sys.argv)
+    result = 0
+    for i in range(1, count):
+        result += int(sys.argv[i])
+    print('{:d}'.format(result))
