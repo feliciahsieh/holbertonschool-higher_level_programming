bb414d91cc88f3061e89fc0fd20afc0052292338
feliciahsieh <felicia@tixwriteoff.com>
2017-08-04 02:01:51 +0000

    rearranged code. Logic is the same

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,12 +4,16 @@ if __name__ == "__main__":
     import sys
     count = len(sys.argv)
     if count == 1:
+        argu = "argument"
+        punc = "."
         print("0 argument.")
         sys.exit(0)
     elif count == 2:
-        print("1 argument:")
+        argu = "argument"
+        punc = ":"
     else:
-        print('{:d} arguments'.format(count - 1))
-
+        argu = "arguments"
+        punc = ":"
+    print('{:d} {:s}{:s}'.format(count - 1, argu, punc), sep="")
     for i in range(1, count):
         print('{:d}: {:s}'.format(i, sys.argv[i]))
