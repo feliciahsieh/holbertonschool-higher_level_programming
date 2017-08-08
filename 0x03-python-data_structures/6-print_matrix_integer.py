f2c44c674bf3178c89a82a0cbaef66e4a07b1cc2
feliciahsieh <felicia@tixwriteoff.com>
2017-08-08 23:15:44 +0000

    removed hardcoded number

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,8 +6,9 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         i = 0
         for col in row:
-            if i == 2:
+            if i == (len(row) - 1):
                 print('{:d}'.format(col))
             else:
                 print('{:d}'.format(col), end=" ")
             i = i + 1
+    print()
