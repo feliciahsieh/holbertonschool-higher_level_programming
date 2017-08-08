f226fab4d5e6129fa7a91dfe29c145c37d4a643d
feliciahsieh <felicia@tixwriteoff.com>
2017-08-08 20:35:46 +0000

    simplify

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 
 
 def element_at(my_list, idx):
-    if idx > (len(my_list) - 1) or abs(idx) >= abs(len(my_list)):
+    if idx < len(my_list):
         return None
     else:
         return my_list[idx]
