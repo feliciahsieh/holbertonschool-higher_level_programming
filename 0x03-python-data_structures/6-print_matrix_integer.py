e2b2407070a9b7fa134118ad59dfb113f83615c0
feliciahsieh <felicia@tixwriteoff.com>
2017-08-08 22:45:26 +0000

    first commit with code

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    i=0;
+    for row in matrix:
+        i = 0
+        for col in row:
+            if i == 2:
+                print('{:d}'.format(col))
+            else:
+                print('{:d}'.format(col), end=" ")
+            i = i + 1
