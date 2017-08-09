28905cfffb3dd38bcce03bcb0613f9d2d01b80b2
feliciahsieh <felicia@tixwriteoff.com>
2017-08-09 02:09:22 +0000

    make sure idx = 0 is ok

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,6 +3,6 @@
 
 def delete_at(my_list=[], idx=0):
     if my_list:
-        if idx < len(my_list) and idx > 0:
+        if idx < len(my_list) and idx >= 0:
             del my_list[idx]
     return my_list
