dbb70b6182737928cf5e17f8a881f0b010497930
Felicia <felicia@tixwriteoff.com>
2017-08-08 19:23:48 -0700

    Delete 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-
-def print_matrix_integer(matrix=[[]]):
-    for sub_list in matrix:
-        if len(sub_list) == 0:
-            print("")
-            continue
-        print(" ".join("{}".format(i) for i in sub_list))
