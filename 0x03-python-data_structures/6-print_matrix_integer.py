67c28b0ff9b5c91b491bd7f74c3ca2760c5ad961
feliciahsieh <felicia@tixwriteoff.com>
2017-08-09 01:29:55 +0000

    add init check back

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,7 @@
 
 
 def print_matrix_integer(matrix=[[]]):
+    if len(matrix) == 1 or len(matrix) == 0:
+        print()
     for element in matrix:
         print(" ".join("{}".format(i) for i in element))
