da638dfb1973b8f3ae4b993371958183252de859
feliciahsieh <felicia@tixwriteoff.com>
2017-08-13 03:53:12 +0000

    shell file to compile 100-print_python_list_info.c task

---
+++ 0x03-python-data_structures/100cpython.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+gcc-4.8 -Wall -Werror -Wextra -pedantic -std=c99 -shared -Wl,-soname,PyList -o libPyList.so -fPIC -I/usr/include/python3.4 100-print_python_list_info.c
+python3 100-test_lists.py
