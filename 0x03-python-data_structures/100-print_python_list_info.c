584e62c91933e4b2a40b3f9b01005f35adb500e4
feliciahsieh <felicia@tixwriteoff.com>
2017-08-13 17:15:04 +0000

    working version

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -3,31 +3,32 @@
 /**
  * printElement - print element info
  * @p: python object
- * @n: number of items in object
+ * @num: number of items in object
  * Return: none
  */
 void printElement(PyObject *p, int num)
 {
 	char *pyTypes[5] = { "str", "int", "float", "tuple", "list" };
 	int i;
-        PyObject *item = NULL;
-
-	pyTypes[0] = pyTypes[0];
-	item = item;
+	PyObject *item = NULL;
 
 	if (num > 0)
 	{
 		for (i = 0; i < num; i++)
 		{
-/*			printf("Element %d: %s\n", i, pyTypes[i]);*/
-
 			item = PyList_GetItem(p, i);
-/*			PyObject_Print(item, stdout, i);*/
-/*			PyObject_Print(type(item), stdout, i);*/
-			printf("\n");
+			if (PyUnicode_Check(item))
+				printf("Element %d: %s\n", i, pyTypes[0]);
+			else if (PyLong_Check(item))
+				printf("Element %d: %s\n", i, pyTypes[1]);
+			else if (PyFloat_Check(item))
+				printf("Element %d: %s\n", i, pyTypes[2]);
+			else if (PyTuple_Check(item))
+				printf("Element %d: %s\n", i, pyTypes[3]);
+			else if (PyList_Check(item))
+				printf("Element %d: %s\n", i, pyTypes[4]);
 		}
 	}
-	printf("\n");
 }
 
 /**
@@ -46,7 +47,7 @@ void print_python_list_info(PyObject *p)
 	number = (int)PyList_Size(p);
 	printf("[*] Size of the Python List = %d\n", number);
 
-	numAllocated = ((PyListObject*)p)->allocated;
-	printf("[*] Allocated = %d\n",numAllocated);
+	numAllocated = ((PyListObject *)p)->allocated;
+	printf("[*] Allocated = %d\n", numAllocated);
 	printElement(p, number);
 }
