6f73c1905f64390672f4277d22484daec51be4ff
feliciahsieh <felicia@tixwriteoff.com>
2017-08-14 02:23:42 +0000

    Latest version. Transfer. not working.

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,5 +1,6 @@
 #include "Python.h"
 
+
 /**
  * printElement - print element info
  * @p: python object
@@ -18,7 +19,9 @@ void printElement(PyObject *p, int num)
 		{
 			item = PyList_GetItem(p, i);
 			if (PyUnicode_Check(item))
+			{
 				printf("Element %d: %s\n", i, pyTypes[0]);
+			}
 			else if (PyLong_Check(item))
 				printf("Element %d: %s\n", i, pyTypes[1]);
 			else if (PyFloat_Check(item))
@@ -29,6 +32,27 @@ void printElement(PyObject *p, int num)
 				printf("Element %d: %s\n", i, pyTypes[4]);
 		}
 	}
+}
+
+/**
+ * print_python_list - Create a C function that prints
+ * basic info about Python lists
+ * @p: python object
+ * Return: none
+ */
+void print_python_list(PyObject *p)
+{
+	int number = 0, numAllocated = 0;
+
+	if (!p)
+		return;
+
+	number = (int)PyList_Size(p);
+	printf("[*] Size of the Python List = %d\n", number);
+
+	numAllocated = ((PyListObject *)p)->allocated;
+	printf("[*] Allocated = %d\n", numAllocated);
+	printElement(p, number);
 }
 
 /**
@@ -52,19 +76,31 @@ void print_python_list_info(PyObject *p)
 	printElement(p, number);
 }
 
-void print_python_list(PyObject *p)
-{
-}
-
+/**
+ * print_python_bytes - Create a C function that prints
+ * basic byte info about Python lists
+ * @p: python object
+ * Return: none
+ */
 void print_python_bytes(PyObject *p)
 {
-	int i, size = 0;
+	int i, size = 0, nBytes = 0;
 	char *myString = "";
 
+	p = p;
+
 	printf("[.] bytes object info\n");
-	printf("size: %d\n", size);
-	printf("trying string: %s?\n", myString);
-	printf("first %d bytes: ", nBytes);
-	for (i = 0; i < nBytes; i++)
-		printf("%x ", myString[i]);
+
+	/*Some type of checking*/
+	if (p)
+	{
+		printf("size: %d\n", size);
+		printf("trying string: %s?\n", myString);
+		printf("first %d bytes: ", nBytes);
+		for (i = 0; i < nBytes; i++)
+			printf("%x ", myString[i]);
+	} else
+	{
+		printf("[ERROR] Invalid Bytes Object\n");
+	}
 }
