3a4c63706eefad6fbcdfd36df8b80c58e228f0fd
feliciahsieh <felicia@tixwriteoff.com>
2017-08-21 21:35:54 +0000

    first commit with code

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,1 +1,13 @@
-/usr/bin/python3
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = 0.0
+        print("Inside result: ", end="")
+        result = int(a) / int(b)
+    except ZeroDivisionError:
+        result = None
+    except:
+        pass
+    finally:
+        print("{}".format(result))
+        return result
