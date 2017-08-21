1e2ef6ea7d4fca53ac63144baf3a1f16000e77e7
feliciahsieh <felicia@tixwriteoff.com>
2017-08-21 23:54:30 +0000

    checked for neg index

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    i = 0
+    if x < 0:
+        return 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
         except IndexError:
+            print()
+            return i
+        except:
             print()
             return i
     print()
