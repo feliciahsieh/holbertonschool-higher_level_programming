7abb17c3bee169265a29896712c8680fca3c5294
feliciahsieh <felicia@tixwriteoff.com>
2017-08-21 23:02:39 +0000

    fixed styling

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import sys
 
+
 def safe_print_integer_err(value):
     result = False
     try:
         print("{:d}".format(value))
         result = True
     except ValueError as err:
-        sys.stderr.write("Exception: %s\n" %str(err))
+        sys.stderr.write("Exception: {}".format(str(err)))
     finally:
         return result
