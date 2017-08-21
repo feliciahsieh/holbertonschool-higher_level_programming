7ba22d6fbe3c6178a2697a3501982b7ab3184b3a
feliciahsieh <felicia@tixwriteoff.com>
2017-08-21 23:46:05 +0000

    moved for loop to outside of try

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for i in range(x):
+    i = 0
+    for i in range(x):
+        try:
             print('{}'.format(my_list[i]), end="")
-        print()
-        i += 1
-    except IndexError:
-        print()
-    return i
+        except IndexError:
+            print()
+            return i
+    print()
+    return i + 1
