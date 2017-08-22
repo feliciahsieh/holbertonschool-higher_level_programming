c8c454c005b5986181a1cc5c388c17fd49cf7228
feliciahsieh <felicia@tixwriteoff.com>
2017-08-22 04:23:28 +0000

    removed except: statement

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    j = 0
     i = 0
     while(i < x):
         try:
             print("{}".format(my_list[i]), end="")
             i += 1
         except IndexError:
-            print()
-            return i
-        except:
             print()
             return i
     print()
