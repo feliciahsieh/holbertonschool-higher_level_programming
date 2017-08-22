e607d5b3b05ce366fa399bf8dfd5089fd48ea6d8
feliciahsieh <felicia@tixwriteoff.com>
2017-08-22 00:20:41 +0000

    checking for neg list_length and removed int()

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     a = []
+    if list_length <= 0:
+        print("out of range")
+        return a
     for i in range(list_length):
         try:
-            result = 0.0
-            result = int(my_list_1[i]) / int(my_list_2[i])
-        except (TypeError, ValueError):
+            result = 0
+            result = my_list_1[i] / my_list_2[i]
+        except (TypeError):
             print("wrong type")
             continue
         except ZeroDivisionError:
