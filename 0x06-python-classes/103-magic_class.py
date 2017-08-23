a1c9a463f294fe3af72171125219c1f418c3223f
feliciahsieh <felicia@tixwriteoff.com>
2017-08-23 06:28:02 +0000

    closer. does not work

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -3,14 +3,15 @@ import dis
 
 
 
+
+
 class MagicClass:
-    def __init__(self):
-        if type(self.__radius) == int:
-            pass
+    def __init__(self): #10
+        self.__radius = 0 #11
+        if type(radius) is not int and type(radius) is not float:
+            return self.__radius == radius
         if type(self.__radius) == float:
-            pass
-
-
+            raise TypeError('radius must be a number')
     def area(self, radius):
         return self.__radius ** 2 * math.pi
 
