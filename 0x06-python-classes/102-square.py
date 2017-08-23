c1846838bab450c1165bc687a60f664e21386017
feliciahsieh <felicia@tixwriteoff.com>
2017-08-23 05:23:31 +0000

    pep8 styling only

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -48,13 +48,18 @@ class Square:
 
     def __eq__(self, other):
         return self.__size == other.__size
+
     def __ne__(self, other):
         return self.__size != other.__size
+
     def __gt__(self, other):
         return self.__size > other.__size
+
     def __lt__(self, other):
         return self.__size < other.__size
+
     def __ge__(self, other):
         return self.__size >= other.__size
+
     def __le__(self, other):
         return self.__size <= other.__size
