d5c50a9f32a2785ac86beaf56fc23b6684a5a440
feliciahsieh <felicia@tixwriteoff.com>
2017-08-24 23:56:50 +0000

    first commit with code

--- 0x07-python-test_driven_development/0-main.py
+++ 0x07-python-test_driven_development/0-main.py
@@ -3,8 +3,5 @@ add_integer = __import__('0-add_integer').add_integer
 
 print(add_integer(1, 2))
 print(add_integer(100, -2))
-print(add_integer(100.3, -2))
-try:
-    print(add_integer(4, "School"))
-except Exception as e:
-    print(e)
+print(add_integer(100.3, -2, 9))
+#print(add_integer(4, "School"))
---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+    index = 0
+    nCol = 0
+    if matrix is not None:
+        nCol = len(matrix[0])
+    for i in matrix:
+        if len(matrix[index]) != nCol:
+            raise TypeError("Each row of the matrix must have the same size")
+        for j in i:
+            if type(j) is not int and type(i) is not float:
+                raise TypeError("matrix must be a matrix (list of lists)\
+                of integers/floats")
+        index += 1
+
+    if type(div) is not int and type(div) is not float:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    result = []
+    row = []
+    for i in matrix:
+        for j in i:
+            row.append(round(j / div, 2))
+        result.append(row)
+        row = []
+
+    return result
