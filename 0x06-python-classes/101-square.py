0a6e3a7798ab4bc61283db0f06eb5f5b8a62f5cc
feliciahsieh <felicia@tixwriteoff.com>
2017-08-24 00:31:34 +0000

    add __repr__ builtin

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -106,3 +106,21 @@ class Square:
             a = a + '\n'
 
         return(a)
+
+    def __repr__(self):
+        """ Prints a square made up of #'s
+        Args:
+            None
+        """
+        a = ""
+        if self.__size != 0:
+            a = a + '\n' * self.__position[1]
+            for i in range(self.__size):
+                a = a + ' ' * self.__position[0]
+                a = a + '#' * self.__size
+                if i < self.__size - 1:
+                    a = a + '\n'
+        else:
+            a = a + '\n'
+
+        return(a)
