0d0f6756dc44e2444e0665108118705b835a77f4
feliciahsieh <felicia@tixwriteoff.com>
2017-08-28 07:29:19 +0000

    works, but unfinished cases

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -14,6 +14,10 @@ def lazy_matrix_mul(m_a, m_b):
     m_b (list): second matrix
     """
 
+    # STILL NEED TO ADD 100-xxx ERROR MSGS
+    if m_a == [] or m_b == []:
+        raise ValueError
+
     return np.matmul(m_a, m_b)
 
 if __name__ == "__main__":
