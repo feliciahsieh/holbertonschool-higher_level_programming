58fe2c43cc394975e8a3accf13c53f0c6ac8f1cf
feliciahsieh <felicia@tixwriteoff.com>
2017-08-28 06:24:41 +0000

    pep8

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,12 +12,12 @@ def add_integer(a, b):
     b (int): second operand
     """
 
-    #Check for Infinite number
+    # Check for Infinite number
     if a == float('inf') or a == -float('inf'):
         return float('inf')
     if b == float('inf') or b == -float('inf'):
         return float('inf')
-    #Check for NaN
+    # Check for NaN
     if a != a or b != b:
         return float('nan')
 
