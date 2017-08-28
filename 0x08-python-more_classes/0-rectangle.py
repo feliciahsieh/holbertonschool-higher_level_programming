ba4d70c9a75e126bd57fd3725ae61a5a27bd10d4
feliciahsieh <felicia@tixwriteoff.com>
2017-08-28 20:54:12 +0000

    first commit with code

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/python3
+"""
+    define class Rectangle
+"""
+
+class Rectangle:
+    """ Define Rectangle class
+
+    Args:
+        None
+    """
+
+    def __init__(self):
+        pass
+
+#if __name__ == "__main__":
+#    from 0-rectangle import Rectangle
