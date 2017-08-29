9ea25658e0311966afe347669b9c788aa967fede
feliciahsieh <felicia@tixwriteoff.com>
2017-08-29 20:47:11 +0000

    fix typo >=0 to >= 0

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -36,7 +36,7 @@ class Rectangle:
         if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value < 0:
-            raise ValueError("width must be >=0")
+            raise ValueError("width must be >= 0")
         else:
             self.__width = value
 
@@ -55,7 +55,7 @@ class Rectangle:
         if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value < 0:
-            raise ValueError("height must be >=0")
+            raise ValueError("height must be >= 0")
         else:
             self.__height = value
 
--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -41,7 +41,7 @@ class Rectangle:
         if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value < 0:
-            raise ValueError("width must be >=0")
+            raise ValueError("width must be >= 0")
         else:
             self.__width = value
 
@@ -60,7 +60,7 @@ class Rectangle:
         if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value < 0:
-            raise ValueError("height must be >=0")
+            raise ValueError("height must be >= 0")
         else:
             self.__height = value
 
