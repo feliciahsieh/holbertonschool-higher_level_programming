7955cd46462ff0902adead844c1ca85833c34977
feliciahsieh <felicia@tixwriteoff.com>
2017-08-30 02:56:08 +0000

    reduced to 3 lines

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def magic_string(n):
-    if n == 0:
-        return ""
-    return "Holberton, " * (n - 1) + "Holberton"
+    return "Holberton, " * (n - 1) + "Holberton" if n != 0 else ""
