9752e79f8d06257cd1e374038b7e0da74f548183
feliciahsieh <felicia@tixwriteoff.com>
2017-08-30 23:34:44 +0000

    even shorter

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    magic_string.counter = getattr(magic_string, "counter", 0) + 1
-    return ", ". join("Holberton" for x in range(magic_string.counter))
+def magic_string(s = []):
+    s += ["Holberton"]
+    return ", ". join(s)
