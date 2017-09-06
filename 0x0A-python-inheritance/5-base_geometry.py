f58ea7e6c2a862f9b52dd0137939116c6d833619
feliciahsieh <felicia@tixwriteoff.com>
2017-09-06 07:43:37 +0000

    first commit with code

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    5-base_geometry.py
+"""
+
+
+class BaseGeometry():
+    """
+    class BaseGeometry definition
+    Args:
+         None
+    Return: None
+    """
+    def __init__(self):
+        """
+        __init__ - initialization of BaseGeometry class
+        Args:
+            None
+        Return: None
+        """
+        pass
