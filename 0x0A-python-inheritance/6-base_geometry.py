2d4128a022849c33d814c6815984f0d0ef64fc21
feliciahsieh <felicia@tixwriteoff.com>
2017-09-06 07:48:09 +0000

    first commit with code

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+    6-base_geometry.py
+"""
+
+
+class BaseGeometry():
+    """
+    class BaseGeometry definition
+    Args:
+         None
+    Return: None
+    """
+    def __init__(self):
+        """
+        __init__ - initialization of BaseGeometry class
+        Args:
+            None
+        Return: None
+        """
+        pass
+
+    def area(self):
+        """
+        area - raises Exception when method is called
+        Args:
+            None
+        Return: None
+        """
+        raise Exception("area() is not implemented")
