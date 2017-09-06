fbd0a1d2e4d391932f09da7a0f9bb7d755f548ca
feliciahsieh <felicia@tixwriteoff.com>
2017-09-06 07:55:53 +0000

    first commit with code

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+    7-base_geometry.py
+"""
+
+
+class BaseGeometry():
+    """
+    class BaseGeometry definition
+    Args:
+         None
+    Return: None
+    """
+    def __init__(self):
+        """
+        __init__ - initialization of BaseGeometry class
+        Args:
+            None
+        Return: None
+        """
+        pass
+
+    def area(self):
+        """
+        area - raises Exception when method is called
+        Args:
+            None
+        Return: None
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        integer_validator - verify value is an integer > 0
+        Args:
+            name (str): variable name
+            value (int): value to store
+        Return: None
+        """
+        if name is None or value is None:
+            return
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
