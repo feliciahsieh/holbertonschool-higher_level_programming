521ba078aa9c5b2e3b65d60998a9d93caf05dc88
feliciahsieh <felicia@tixwriteoff.com>
2017-09-06 06:10:35 +0000

    first commit with code

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+3-is_kind_of_class.py / is_kind_of_class module
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    is_kind_of_class(obj, a_class):
+    Args:
+        obj (object): object to inspect
+        a_class (classname): classname to compare
+    Returns:
+        True if the object is an instance of,
+    or if the object is an instance of a class that inherited from
+    the specified class;
+        otherwise False.
+    """
+
+    if issubclass(type(obj), a_class):
+        return True
+    return False
