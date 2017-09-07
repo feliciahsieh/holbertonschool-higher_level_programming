ca4fe3ccf9a4c15e1d12887964e7265e5c52165a
feliciahsieh <felicia@tixwriteoff.com>
2017-09-07 01:47:34 +0000

    first commit with code

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+    10-square class
+    Note: must use specia __import__ to get file due to hyphen problem
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    class Square definition
+    Args:
+         None
+    Return: None
+    """
+
+    __size = 0
+
+    def __init__(self, size):
+        """
+        __init__ - initialization of Square class
+        Args:
+            None
+        Return: None
+        """
+
+        if size > 0:
+            self.integer_validator("size", size)
+            super().__init__(size, size)
+            self.__size = size
+
+    def area(self):
+        """
+        area - calculates area of Square
+        Args:
+             None
+        Returns:
+             Area of Square
+        """
+        return self.__size * self.__size
---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+    9-rectangle.py / class Rectangle
+    Note: must use specia __import__ to get file due to hyphen problem
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    class Rectangle definition
+    Args:
+         None
+    Return: None
+    """
+    def __init__(self, width, height):
+        """
+        __init__ - initialization of Rectangle class
+        Args:
+            None
+        Return: None
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        area - Rectangle method to calculate area
+
+        Args:
+            None
+
+        Return: area of Rectangle
+        """
+        return self.__width * self.__height
+
+    def print(self):
+        """
+        print - print the Rectangle area
+        Arg:
+            None
+        Return: area of Rectangle
+        """
+        return self.area
+
+    def __str__(self):
+        """
+        __str__ - print the formal Rectangle data
+        Arg:
+            None
+        Return: string describing area
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
