4d3b672c8c51b73f551587290619fe5f6a99f074
feliciahsieh <felicia@tixwriteoff.com>
2017-09-07 23:34:38 +0000

    changed chmod for main files and first commit with code for task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def read_file(filename=""):
+    """
+    read_file - reads a text file with UTF8 encoding and prints it to stdout
+
+    Args:
+        filename(str): filename to open
+    Returns
+        None
+    """
+
+    with open(filename, mode="r+", encoding="UTF8") as f:
+        for line in f:
+            print(line, end="")
+    f.close()
