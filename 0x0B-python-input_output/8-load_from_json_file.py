79eb5b91f07e6292426fd36f3b82ad4a45b935fd
feliciahsieh <felicia@tixwriteoff.com>
2017-09-08 03:56:52 +0000

    first commit with code. Doesn't work

---
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+8-load_from_json_file.py / load_from_json_file -
+creates an Object from a "JSON file
+"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    load_from_json_file - creates an Object from a "JSON file
+    Args
+        filename(str) - filename to read from
+    Return
+        None
+    """
+    if type(filename) is not str:
+        return
+
+    with open(filename, mode="r") as f:
+        myObj = f.read()
+    x = json.loads(myObj)
+    return x
