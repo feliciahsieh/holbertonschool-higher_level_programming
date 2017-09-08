77de8c1abb7ff0ada2d2d7b1c251a0673776353e
feliciahsieh <felicia@tixwriteoff.com>
2017-09-08 03:20:16 +0000

    first commit with code

---
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+7-save_to_json_file.py / save_to_json_file - write object to a text file
+using a JSON representation
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    save_to_json_file - writes object to text file using a JSON representation
+    """
+
+    if type(filename) is not str:
+        return
+    jsonStr = json.dumps(my_obj)
+
+    with open(filename, "w") as f:
+        f.write(jsonStr)
+        f.close()
