8662434a4be8a4282aef70309da8e2adfbf524cc
feliciahsieh <felicia@tixwriteoff.com>
2017-09-08 07:51:45 +0000

    working

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,21 +1,18 @@
-#!/bin/usr/python3
-"""
-9-add_item.py - adds all arguments to a Python list, and save them to a file.
-Uses:
-save_to_json_file from 7-save_to_json_file.py and
-load_from_json_file from 8-load_from_json_file.py
-"""
-
+#!/usr/bin/python3
 import sys
 import json
+import os
 
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 fname = "add_item.json"
 
-with open('add_item.json', 'a+') as f:
+with open(fname, 'a+') as f:
     pass
 
+if os.stat(fname).st_size == 0:
+    listObj = []
+else:
     listObj = load_from_json_file(fname)
 
 for i in range(1, len(sys.argv)):
