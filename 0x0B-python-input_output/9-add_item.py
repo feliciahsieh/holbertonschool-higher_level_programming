8bda793acd6bdb57aef56c4b76fdc88630e506d8
feliciahsieh <felicia@tixwriteoff.com>
2017-09-08 04:04:57 +0000

    first commit. Doesn't work

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,15 @@
+#!/bin/usr/python3
+"""
+9-add_item.py - adds all arguments to a Python list, and save them to a file.
+Uses:
+save_to_json_file from 7-save_to_json_file.py and
+load_from_json_file from 8-load_from_json_file.py
+"""
+
+import json
+
+with open("7-save_to_jason_file.py", "r") as f7:
+
+with open("8-load_from_json_file.py", "r") as j8:
+
+with open("add_item.json", "w") as f:
