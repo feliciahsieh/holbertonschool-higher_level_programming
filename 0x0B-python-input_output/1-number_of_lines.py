c3988dbc5047a9c7d4f5352e040b1194c5c06871
feliciahsieh <felicia@tixwriteoff.com>
2017-09-08 01:10:49 +0000

    remove f.close()

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -4,6 +4,7 @@
 returns the number of lines of a text file
 """
 
+
 def number_of_lines(filename=""):
     """
     number_of_lines - returns the number of lines of a text file
@@ -20,6 +21,4 @@ def number_of_lines(filename=""):
     with open(filename, mode="r", encoding="utf-8") as f:
         for i, l in enumerate(f):
             pass
-    f.close()
-
     return i + 1
