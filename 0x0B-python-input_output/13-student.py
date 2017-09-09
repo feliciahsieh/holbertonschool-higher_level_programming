bfdff5eedd538963033d37df450b061452d08d6e
feliciahsieh <felicia@tixwriteoff.com>
2017-09-09 20:40:14 +0000

    first commit with code

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+
+class Student:
+    """
+    Student class definition
+    Member:
+        first_name (str) - first name of student
+        last_name (str) - last name of student
+        age (int) - age of student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        __init__ - initialization of class instance
+        Args:
+            first_name(str) - first name of student
+            last_name(str) - last name of student
+            age(int) - age of student
+        Return:
+            None
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        to_json - return dictionary of self
+        Args:
+            None
+        Return:
+            python dictionary of self
+        """
+
+        d = {}
+
+        if attrs is not None:
+            for k in self.__dict__.keys():
+                for a in range(len(attrs)):
+                    if attrs[a] == k:
+                        d[k] = self.__dict__[k]
+        else:
+            for k in self.__dict__.keys():
+                if type(k) == str:
+                    d[k] = self.__dict__[k]
+        return d
+
+    def reload_from_json(self, json):
+        """
+        reload_from_json - replaces all attributes of Student instance
+        with json struct
+        Args:
+            json(dict) - json struct of student
+        Return:
+            None
+        """
+        self.__dict__.update(json)
