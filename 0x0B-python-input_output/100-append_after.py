f3a2260b1e341439b15b3d07cc0b66f3e5c92c10
feliciahsieh <felicia@tixwriteoff.com>
2017-09-09 21:39:52 +0000

    first commit with code

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def append_after(filename="", search_string="", new_string=""):
+    """
+    append_after - inserts a line of text to a file after each line
+    containing a specific string
+    Args:
+        filename(str) - filename to process
+        search_string(str) - string to search within file
+        new_string(str) - string to add after a line matches search_string
+    Return:
+        None (Change file to reflect changes)
+    """
+    with open(filename, "r") as f:
+        contents = f.readlines()
+
+        for i in range(len(contents)):
+            if search_string in contents[i]:
+                contents.insert(i+1, new_string)
+
+    with open(filename, "w") as f:
+        contents = "".join(contents)
+        f.write(contents)
