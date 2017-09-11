8b83bc7a319583bfc7cdc1166e4bf69b48dc07f4
feliciahsieh <felicia@tixwriteoff.com>
2017-09-11 04:46:06 +0000

    working version of #11 Square size. Added __size instance

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,6 +8,7 @@ class Square(Rectangle):
     Methods:
         __init__()
         __str__()
+        __size()
     """
 
     def __init__(self, size, x=0, y=0, id=None):
@@ -22,6 +23,29 @@ class Square(Rectangle):
             None
         """
         super().__init__(size, size, x, y, id)
+        self.__size = size
+
+    @property
+    def size(self):
+        """ Getter for size """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        size(value) - Setter for size
+        Args:
+            value(int) - size of Square
+        Return:
+            None
+        """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
+        self.__size = value
 
     def __str__(self):
         """
