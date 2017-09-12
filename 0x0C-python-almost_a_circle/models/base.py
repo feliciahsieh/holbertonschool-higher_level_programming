30876fc20ab181f18dc34c830cfcaccd95fe00e7
feliciahsieh <felicia@tixwriteoff.com>
2017-09-12 00:19:50 +0000

    working version of #16 JSON string to file but not optimal

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Base class definition
+"""
+import json
 
 
 class Base:
@@ -21,6 +25,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """
         to_json_string - return JSON string of list_dictionaries
@@ -33,3 +38,32 @@ class Base:
             return "[]"
 
         return str(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        save_to_file - writes JSON string of list_objs to a file.
+        Convert (Rectangle|Square obj) -> dictionary -> string of dict -> file.
+        Args:
+            cls() - class type of list_objs. Used to derive <ClassName>.json
+            list_objs(list) - list of Base instances (Rectangle or Square)
+        Return:
+            None
+        """
+
+        #a = {}
+
+        fs = ""
+        l = []
+        i = 0
+        for obj in list_objs:
+            s = Base.to_json_string(obj.to_dictionary())
+            if (i != 0):
+                fs = fs + ", " + s
+            else:
+                fs = "[" + s
+                i = 1
+            print(fs)
+        fs = fs + "]"
+        with open(str(cls.__name__) + ".json", "w+") as f:
+            f.write(str(fs))
