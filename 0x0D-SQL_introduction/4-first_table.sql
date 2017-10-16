be93caa20e803e60275776bd0ec21c694bdfa222
feliciahsieh <felicia@tixwriteoff.com>
2017-10-16 22:48:00 +0000

    remove db name from consideration

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- Create a table, first_table
-IF NOT EXISTS hbtn_0c_0 (CREATE TABLE first_table (id INT, name VARCHAR(256) ));
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,2 @@
+-- Print full description of table, first_tablel, from database, hbtn_0c_0
+SHOW COLUMNS FROM first_table;
