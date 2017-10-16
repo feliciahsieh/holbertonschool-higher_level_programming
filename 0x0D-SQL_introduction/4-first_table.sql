4f275cfa9fcc2db862cecfbe1983fd36b2bcd473
feliciahsieh <felicia@tixwriteoff.com>
2017-10-16 22:37:44 +0000

    compacted text

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,8 +1,2 @@
 -- Create a table, first_table
-IF NOT EXISTS hbtn_0c_0
-   BEGIN
-	CREATE TABLE first_table (
-      	        id INT,
-		name VARCHAR(256)
-	);
-   END
+IF NOT EXISTS hbtn_0c_0 (CREATE TABLE first_table (id INT, name VARCHAR(256) ));
