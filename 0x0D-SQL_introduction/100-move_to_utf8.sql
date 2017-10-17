867240eacc36f027b64fdbe76a768ca6602ed823
feliciahsieh <felicia@tixwriteoff.com>
2017-10-17 05:26:35 +0000

    change back to utf8mb4

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,19 +2,19 @@
 -- For each database:
 ALTER DATABASE
     hbtn_0c_0
-    CHARACTER SET = utf8
-    COLLATE = utf8_unicode_ci;
+    CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_unicode_ci;
 
 -- For each table:
 ALTER TABLE
     first_table
-    CONVERT TO CHARACTER SET utf8
-    COLLATE utf8_unicode_ci;
+    CONVERT TO CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
 
 -- For each column:
 ALTER TABLE
     first_table
     CHANGE name name
     VARCHAR(256)
-    CHARACTER SET utf8
-    COLLATE utf8_unicode_ci;
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
