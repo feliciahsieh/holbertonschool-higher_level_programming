2fdc5a304ccf9dc4c03d50fa075aa08f6be2927e
feliciahsieh <felicia@tixwriteoff.com>
2017-10-17 00:35:29 +0000

    Was able to rename column name

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,2 +1,2 @@
 -- Compute score average of all records in table, second_table, in database, hbtn_0c_0
-SELECT AVG(score) FROM second_table AS average;
+SELECT AVG(score) AS average FROM second_table;
