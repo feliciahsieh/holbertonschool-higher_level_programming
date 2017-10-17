48bb0a64a5a74d8cb6960078aaa3ae2dc5b148e2
feliciahsieh <felicia@tixwriteoff.com>
2017-10-17 00:53:43 +0000

    renamed column

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
 -- List number of records with same score in table, second_table, in database hbtn_0c_0
-SELECT score, COUNT(score) FROM second_table AS C
+SELECT score, COUNT(score) AS number FROM second_table AS C
 GROUP BY score
 ORDER BY score DESC;
