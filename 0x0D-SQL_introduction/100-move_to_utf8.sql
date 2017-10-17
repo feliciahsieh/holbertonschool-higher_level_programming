85d2597a803fa6a15ec78dd7112c6edd2f54fbc3
feliciahsieh <felicia@tixwriteoff.com>
2017-10-17 05:19:32 +0000

    fixed db name and changed to utf8 instead of utf8mb4

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,20 +2,20 @@
 
 -- For each database:
 ALTER DATABASE
-    hbtn_0C_0
-    CHARACTER SET = utf8mb4
-    COLLATE = utf8mb4_unicode_ci;
+    hbtn_0c_0
+    CHARACTER SET = utf8
+    COLLATE = utf8_unicode_ci;
 
 # For each table:
 ALTER TABLE
     first_table
-    CONVERT TO CHARACTER SET utf8mb4
-    COLLATE utf8mb4_unicode_ci;
+    CONVERT TO CHARACTER SET utf8
+    COLLATE utf8_unicode_ci;
 
 # For each column:
 ALTER TABLE
     first_table
     CHANGE name name
     VARCHAR(256)
-    CHARACTER SET utf8mb4
-    COLLATE utf8mb4_unicode_ci;
+    CHARACTER SET utf8
+    COLLATE utf8_unicode_ci;
