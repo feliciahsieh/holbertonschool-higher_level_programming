26999233cbc3236844190adaebf816ba52542115
feliciahsieh <felicia@tixwriteoff.com>
2017-10-17 02:45:29 +0000

    first commit with code

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,8 @@
 -- Convert hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_general_ci;
+SELECT default_character_set_name FROM information_schema.schemata WHERE schema_name = "first_table";
+
 UPDATE first_table
 SET name = CONVERT(name USING utf8);
+
+UPDATE first_table SET name = CONVERT(CONVERT(CONVERT(name USING latin1) USING binary) USING UTF8);
