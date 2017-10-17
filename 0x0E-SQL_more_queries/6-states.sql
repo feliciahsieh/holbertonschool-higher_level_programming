3340a2a8c60eaefb901191545a298c1fbc3f472d
feliciahsieh <felicia@tixwriteoff.com>
2017-10-17 22:44:27 +0000

    change database name

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,8 @@
 -- Create database, hbtn_0d_usa, and table states (in database, hbtn_0d_usa)
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+USE hbtn_0d_usa;
+
 CREATE TABLE IF NOT EXISTS states (
        id INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT,
        name VARCHAR(256) NOT NULL
