7833ab2190047a0918167f47547ba8cdb565d691
feliciahsieh <felicia@tixwriteoff.com>
2017-10-17 05:09:58 +0000

    updated

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,9 +1,21 @@
 -- Convert hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_general_ci;
-SELECT default_character_set_name FROM information_schema.schemata WHERE schema_name = "hbtn_c0_0";
 
-UPDATE first_table
-SET name = CONVERT(name USING utf8);
+-- For each database:
+ALTER DATABASE
+    hbtn_0C_0
+    CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_unicode_ci;
 
-UPDATE first_table
-SET name = CONVERT(CONVERT(CONVERT(name USING latin1) USING binary) USING UTF8);
+# For each table:
+ALTER TABLE
+    first_table
+    CONVERT TO CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+
+# For each column:
+ALTER TABLE
+    first_table
+    CHANGE name name
+    VARCHAR(256)
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
