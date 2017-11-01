7495b0b6f1e25e33e375c98ad1f4d40738110853
feliciahsieh <felicia@tixwriteoff.com>
2017-11-01 01:45:35 +0000

    first commit with code

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,6 @@
+-- List all shows from hbtn_0d_tvshows_rate by their rating
+SELECT title, sum(rate) FROM tv_shows
+JOIN tv_show_ratings ON id
+WHERE id=show_id
+GROUP BY id
+ORDER BY sum(rate) DESC;
