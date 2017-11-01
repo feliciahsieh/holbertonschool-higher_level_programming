37b4f14de0411e601a122933b3a96d97405ea941
feliciahsieh <felicia@tixwriteoff.com>
2017-11-01 02:17:19 +0000

    UPPER CASED SUM()

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,8 +1,8 @@
 -- List all shows from hbtn_0d_tvshows_rate by their rating
 USE hbtn_0d_tvshows_rate;
 
-SELECT title, sum(rate) AS rating FROM tv_shows
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating FROM tv_shows
 JOIN tv_show_ratings ON id
 WHERE id=show_id
 GROUP BY id
-ORDER BY sum(rate) ASC;
+ORDER BY rating ASC;
