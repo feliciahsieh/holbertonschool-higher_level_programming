05b586bb1c9c1931814a8c3fb2c0caa91b9524a6
feliciahsieh <felicia@tixwriteoff.com>
2017-11-01 02:26:07 +0000

    No change in logic. Making everything explicit

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -5,6 +5,6 @@ SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows
 JOIN tv_show_ratings
 ON tv_shows.id
-WHERE id=show_id
+WHERE tv_shows.id=tv_show_ratings.show_id
 GROUP BY tv_shows.id
 ORDER BY rating DESC;
