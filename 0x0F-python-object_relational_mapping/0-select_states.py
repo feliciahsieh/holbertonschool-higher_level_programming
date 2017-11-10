70dc4358351324c4659e355a6844e101db1e339c
feliciahsieh <felicia@tixwriteoff.com>
2017-11-10 14:47:18 +0000

    removed object-oriented code

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,38 +2,23 @@
 """
     file: 0-select_states.py
 """
-import MySQLdb
-
-
-class MyStates:
-        """
-        mysql table states
-        """
-
-        def printStates(*arg):
-                """
-                Display all states in mySQL table, states
-                Args:
-                    args: arguments being passed in
-                """
-
-                db = MySQLdb.connect(
-                        host="localhost",
-                        port=3306,
-                        user=arg[1],
-                        passwd=arg[2],
-                        db=arg[3])
-                cur = db.cursor()
-
-                cur.execute("SELECT * FROM states ORDER BY id ASC")
-                rows = cur.fetchall()
-                for row in rows:
-                        print(row)
-
-                cur.close()
-                db.close()
 
 if __name__ == "__main__":
-    from sys import argv
+        import MySQLdb
+        from sys import argv
 
-MyStates.printStates(*argv)
+        db = MySQLdb.connect(
+                host="localhost",
+                port=3306,
+                user=argv[1],
+                passwd=argv[2],
+                db=argv[3])
+        cur = db.cursor()
+
+        cur.execute("SELECT * FROM states ORDER BY id ASC")
+        rows = cur.fetchall()
+        for row in rows:
+                print(row)
+
+        cur.close()
+        db.close()
