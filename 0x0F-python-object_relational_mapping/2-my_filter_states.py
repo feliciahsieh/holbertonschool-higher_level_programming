434d622769a07a3f2d3ee64952bb6a0af9c5d900
feliciahsieh <felicia@tixwriteoff.com>
2017-11-10 15:00:00 +0000

    removed object-oriented code

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,41 +2,23 @@
 """
     file: 2-my_filter_states.py
 """
-import MySQLdb
-
-
-class MyStates:
-        """
-        mysql table states
-        """
-
-        def printMatchingStates(*arg):
-                """
-                Display all states that match user input in mySQL table, states
-                Args:
-                    args: arguments being passed in
-                """
-
-                db = MySQLdb.connect(
-                        host="localhost",
-                        port=3306,
-                        user=arg[1],
-                        passwd=arg[2],
-                        db=arg[3])
-                cur = db.cursor()
-
-                t = arg[4]
-                cur.execute(
-                        "SELECT * FROM states WHERE name=(%s) \
-ORDER BY id ASC", (t,))
-                rows = cur.fetchall()
-                for row in rows:
-                        print(row)
-
-                cur.close()
-                db.close()
-
 if __name__ == "__main__":
     from sys import argv
+    import MySQLdb
 
-MyStates.printMatchingStates(*argv)
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3])
+    cur = db.cursor()
+
+    t = argv[4]
+    cur.execute("SELECT * FROM states WHERE name=(%s) \
+ORDER BY id ASC", (t,))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
