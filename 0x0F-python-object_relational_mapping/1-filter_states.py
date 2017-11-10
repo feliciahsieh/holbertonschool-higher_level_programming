57b637f11ee1da28b38030770d017231e59c565c
feliciahsieh <felicia@tixwriteoff.com>
2017-11-10 14:56:38 +0000

    changed size of tabs only

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,22 +4,21 @@
 """
 
 if __name__ == "__main__":
-        import MySQLdb
-        from sys import argv
+    import MySQLdb
+    from sys import argv
 
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3])
+    cur = db.cursor()
 
-        db = MySQLdb.connect(
-                host="localhost",
-                port=3306,
-                user=argv[1],
-                passwd=argv[2],
-                db=argv[3])
-        cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
 
-        cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-        rows = cur.fetchall()
-        for row in rows:
-                print(row)
-
-        cur.close()
-        db.close()
+    cur.close()
+    db.close()
