a0a82c156b118ab085038d5058fb89fdd8efd70a
feliciahsieh <felicia@tixwriteoff.com>
2017-11-10 15:56:06 +0000

    removed blank lines

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,11 +11,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
-
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
-
     cur.close()
     db.close()
