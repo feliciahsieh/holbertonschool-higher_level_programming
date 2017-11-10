9439f1f16f024572c60b88f3bf51c465ffc0a6e9
feliciahsieh <felicia@tixwriteoff.com>
2017-11-10 18:48:54 +0000

    changed %s to {:s}

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
 
     try:
         db = MySQLdb.connect(host="localhost", port=3306,
-            user=argv[1], passwd=argv[2], db=argv[3])
+                             user=argv[1], passwd=argv[2], db=argv[3])
         cur = db.cursor()
-        cur.execute("SELECT * FROM states WHERE name LIKE %s \
+        cur.execute("SELECT * FROM states WHERE name LIKE {:s} \
         ORDER BY id ASC".format((argv[4],)))
         rows = cur.fetchall()
         for row in rows:
