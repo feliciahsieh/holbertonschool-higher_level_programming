1a9e65dd0254bae28192381aeb1f1e0186cd0222
feliciahsieh <felicia@tixwriteoff.com>
2017-11-10 18:38:48 +0000

    change = to LIKE

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,20 +2,19 @@
 """
     file: 2-my_filter_states.py
 """
-if __name__ == "__main__":
-    from sys import argv
-    import MySQLdb
 
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
     if len(argv) != 5:
         exit(0)
 
     try:
-        db = MySQLdb.connect(
-            host="localhost", port=3306,
+        db = MySQLdb.connect(host="localhost", port=3306,
             user=argv[1], passwd=argv[2], db=argv[3])
         cur = db.cursor()
-
-        cur.execute("SELECT * FROM states WHERE name=(%s) \
+        cur.execute("SELECT * FROM states WHERE name LIKE %s \
         ORDER BY id ASC".format((argv[4],)))
         rows = cur.fetchall()
         for row in rows:
