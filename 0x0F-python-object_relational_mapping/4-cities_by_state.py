f6093744cf8043fe848eb0fc2b702ade96b8fe51
feliciahsieh <felicia@tixwriteoff.com>
2017-11-10 18:25:34 +0000

    no logic changes

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -2,43 +2,19 @@
 """
     file: 4-cities_by_state.py
 """
-import MySQLdb
-
-
-class MyStates:
-        """
-        mysql table states
-        """
-
-        def printCities(*arg):
-                """
-                Display all states that match user input in mySQL table, states
-                Args:
-                    args: arguments being passed in
-                """
-
-                db = MySQLdb.connect(
-                        host="localhost",
-                        port=3306,
-                        user=arg[1],
-                        passwd=arg[2],
-                        db=arg[3])
-                cur = db.cursor()
-
-                cur.execute(
-                        "SELECT cities.id, cities.name, states.name \
-FROM cities \
-INNER JOIN states ON cities.state_id \
-WHERE cities.state_id=states.id \
-ORDER BY cities.id ASC")
-                rows = cur.fetchall()
-                for row in rows:
-                        print(row)
-
-                cur.close()
-                db.close()
-
 if __name__ == "__main__":
     from sys import argv
+    import MySQLdb
 
-MyStates.printCities(*argv)
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities \
+JOIN states ON cities.state_id \
+WHERE cities.state_id=states.id \
+ORDER BY cities.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
