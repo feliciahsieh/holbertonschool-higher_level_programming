61c16f3b5b49a1796faa57c329e26cff257d40c1
feliciahsieh <felicia@tixwriteoff.com>
2017-11-10 19:26:49 +0000

    changes

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,8 +3,8 @@
     file: 2-my_filter_states.py
 """
 
-from sys import argv
 import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
     if len(argv) != 5:
@@ -15,7 +15,7 @@ if __name__ == "__main__":
                              user=argv[1], passwd=argv[2], db=argv[3])
         cur = db.cursor()
         cur.execute("SELECT * FROM states WHERE name LIKE {:s} \
-        ORDER BY id ASC".format((argv[4],)))
+ORDER BY id ASC".format(argv[4]))
         rows = cur.fetchall()
         for row in rows:
             print(row)
