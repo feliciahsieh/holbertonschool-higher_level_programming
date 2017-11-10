6bcebda97cbdea7dc217ce7eee5f9019276ba796
feliciahsieh <felicia@tixwriteoff.com>
2017-11-10 05:46:29 +0000

    working version

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -26,14 +26,20 @@ class MyStates:
                 cur = db.cursor()
 
                 st = arg[4]
-                cur.execute(
-"SELECT cities.name FROM cities \
-WHERE cities.state_id in (SELECT states.id FROM states WHERE states.name='Texas') \
-ORDER BY cities.id ASC")
+                cur.execute("SELECT cities.name FROM cities \
+WHERE cities.state_id in ( \
+SELECT states.id FROM states WHERE states.name=%s) \
+ORDER BY cities.id ASC", (st,))
                 rows = cur.fetchall()
+                i = 0
                 for row in rows:
-                        print(row)
-
+                        if (i != 0):
+                                print(", ", sep="", end="")
+                        else:
+                                i = 1
+                        for col in row:
+                                print("{}".format(col), sep="", end="")
+                print()
                 cur.close()
                 db.close()
 
