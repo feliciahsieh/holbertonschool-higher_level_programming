3d79fe2ddeebf6a7fafffa1a726d72f44a7e1f33
feliciahsieh <felicia@tixwriteoff.com>
2017-11-10 18:09:06 +0000

    add try/except, check for #args

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
 """
-    file: 3-my_safe_filter_states.py
+    3-my_safe_filter_states.py
 """
 if __name__ == "__main__":
-    from sys import argv
     import MySQLdb
+    from sys import argv
 
-db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                     passwd=argv[2], db=argv[3])
-cur = db.cursor()
-t = argv[4]
-cur.execute("SELECT * FROM states \
-WHERE name=(%s) ORDER BY id ASC".format(), (t,))
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-cur.close()
-db.close()
+
+if len(argv) != 5:
+    exit(0)
+
+try:
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states \
+    WHERE name=(%s) ORDER BY id ASC".format(), (argv[4],))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+        cur.close()
+        db.close()
+except:
+    pass
