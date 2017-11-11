16f6676566cc480aeacecb0b394614174bc40d9a
feliciahsieh <felicia@tixwriteoff.com>
2017-11-11 03:54:49 +0000

    first commit with code

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Change the name of the State where id = 2 to New Mexico
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    Base = declarative_base()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.query(State).filter(State.id == 2).update({"name": "New Mexico"})
+    session.commit()
