6749728c4b13aa9aab3edf3cc0f4765b93560269
feliciahsieh <felicia@tixwriteoff.com>
2017-11-21 23:59:51 +0000

    2nd revision

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 # Send a GET request to URL and display body of the response
 curl -sw "%{http_code}" $1
+
+#!/bin/bash
+# Send a request to a URL and display size fo the response body
+curl -Gs $1
+if [ curl -sw "%{http_code}" -o /dev/null 0.0.0.0:5000 -eq 200 ]
+then
+    curl -sG 0.0.0.0:5000
+fi
