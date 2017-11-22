0926eb0b7ec37f9879a407711698c6a547ac8c53
feliciahsieh <felicia@tixwriteoff.com>
2017-11-22 03:50:35 +0000

    new alg for local peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,8 +5,14 @@
 def find_peak(list_of_integers):
     if list_of_integers is None:
         return float('-inf')
+    if len(list_of_integers) == 1):
+        return list_of_integers[0]
     max = list_of_integers[0]
-    for i in list_of_integers:
-        if max < i:
+    if max > list_of_integers[1]:
+        return max
+    for k, v in list_of_integers[1:-2]:
+        if max < v:
             max = i
+        if max > list_of_integers[k + 1]:
+            return max
     return max
