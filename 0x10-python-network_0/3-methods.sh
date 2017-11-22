b98b8bc949f9b9d30afc8e92d0281cf1ea0fa3fd
feliciahsieh <felicia@tixwriteoff.com>
2017-11-22 01:20:29 +0000

    Bug fix. change to user's input

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Display all HTTP methods server will accept for user's URL
-curl -sIX OPTIONS 0.0.0.0:5000/route_4 | awk -F': ' '/Allow/ { print $2 }'
+curl -sIX OPTIONS $1 | awk -F': ' '/Allow/ { print $2 }'
