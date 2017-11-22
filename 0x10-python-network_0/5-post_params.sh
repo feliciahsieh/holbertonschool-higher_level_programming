4a17276ba16c8e9f392188bdffe3f4e9a22169bf
feliciahsieh <felicia@tixwriteoff.com>
2017-11-22 02:16:39 +0000

    using

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a POST request to User's URL w/2 vars and display the body of the response
-curl -sX POST --data "email: hr@holbertonschool.com&subject: I will always be here for PLD" 0.0.0.0:5000/route_6
+# Send a POST request to User's URL w/2 vars & display the body of the response
+curl -sX POST --data "email: hr@holbertonschool.com&subject: I will always be here for PLD" $1
