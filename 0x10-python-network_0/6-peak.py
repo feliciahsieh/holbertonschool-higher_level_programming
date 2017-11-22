e5b16e853b77a385c94e3fa347873ba82a318244
feliciahsieh <felicia@tixwriteoff.com>
2017-11-22 03:55:53 +0000

    reverting

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,10 +5,8 @@
 def find_peak(list_of_integers):
     if list_of_integers is None:
         return float('-inf')
-    if len(list_of_integers) == 1):
-        return list_of_integers[0]
     max = list_of_integers[0]
-    for i in list_of_integers[1:]:
+    for i in list_of_integers:
         if max < i:
             max = i
     return max
