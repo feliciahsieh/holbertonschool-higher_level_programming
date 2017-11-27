dd73a4576d84e0d05ab8cbf25d313a86907a2faa
feliciahsieh <felicia@tixwriteoff.com>
2017-11-27 22:24:58 +0000

    first commit with code

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" 4-hbtn_status.py """
+if __name__ == "__main__":
+    """ __main__ """
+    import requests
+
+    url = 'https://intranet.hbtn.io/status'
+    r = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
