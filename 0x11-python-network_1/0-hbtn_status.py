4d36412fe1e45cbd2c163a5f8c262cec631fe46f
feliciahsieh <felicia@tixwriteoff.com>
2017-11-28 01:43:43 +0000

    add try/except

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 """ 0-hbtn_status.py """
 if __name__ == "__main__":
-    """__main__"""
+    """ __main__ """
     import urllib.request
 
     url = 'https://intranet.hbtn.io/status/'
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+    try:
+        with urllib.request.urlopen(url) as response:
+            html = response.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(html)))
+            print("\t- content: {}".format(html))
+            print("\t- utf8 content: {}".format(html.decode('utf-8')))
+    except:
+        pass
