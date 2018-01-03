93bb42256361c1439a818dea8dc2ec8d47c80768
feliciahsieh <felicia@tixwriteoff.com>
2018-01-03 22:54:59 +0000

    first working version

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+let args = process.argv.slice(2);
+if (args.length === 0) {
+  console.log('0');
+} else {
+  let largest = args[0];
+  let nextLargest = 0;
+  for (let j = 0; j < args.length; j++) {
+    if (args[j] > largest) {
+      nextLargest = largest;
+      largest = args[j];
+    }
+  }
+  console.log(nextLargest);
+}
