0a988f3b8c73ad749fedbff7ce73361a332b7196
feliciahsieh <felicia@tixwriteoff.com>
2018-01-03 23:01:09 +0000

    adjust for duplicate high values

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,7 +6,7 @@ if (args.length < 2) {
   let largest = args[0];
   let nextLargest = 0;
   for (let j = 1; j < args.length; j++) {
-    if (args[j] > largest) {
+    if (args[j] >= largest) {
       nextLargest = largest;
       largest = args[j];
     }
