7777c304e6d2c7d7919198f7db98e9ca8d043c2c
feliciahsieh <felicia@tixwriteoff.com>
2018-01-03 23:35:41 +0000

    add check if target number is greater than nextLargest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,11 +4,15 @@ if (args.length < 2) {
   console.log('0');
 } else {
   let largest = args[0];
-  let nextLargest = 0;
+  let nextLargest;
   for (let j = 1; j < args.length; j++) {
-    if (args[j] >= largest) {
+    if (args[j] > largest) {
       nextLargest = largest;
       largest = args[j];
+    } else {
+      if (args[j] > nextLargest) {
+        nextLargest = args[j];
+      }
     }
   }
   console.log(nextLargest);
