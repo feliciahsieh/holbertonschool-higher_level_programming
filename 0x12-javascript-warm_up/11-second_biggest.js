686ef6ce257d4415e2d46033785cf3796b2c9917
feliciahsieh <felicia@tixwriteoff.com>
2018-01-03 23:40:58 +0000

    adding length===2 check back

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,16 @@
 #!/usr/bin/node
 let args = process.argv.slice(2);
+let largest = args[0];
+let nextLargest;
 if (args.length < 2) {
   console.log('0');
+} else if (args.length === 2) {
+  if (args[0] > args[1]) {
+    nextLargest = args[1];
+  } else {
+    nextLargest = args[0];
+  }
 } else {
-  let largest = args[0];
-  let nextLargest;
   for (let j = 1; j < args.length; j++) {
     if (args[j] > largest) {
       nextLargest = largest;
