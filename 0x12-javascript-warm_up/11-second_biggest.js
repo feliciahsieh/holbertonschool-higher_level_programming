9c3b17bcfa353052cc413c49ef2408c5b9fc45cb
feliciahsieh <felicia@tixwriteoff.com>
2018-01-03 22:59:47 +0000

    fix bug with 1 argument

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 let args = process.argv.slice(2);
-if (args.length === 0) {
+if (args.length < 2) {
   console.log('0');
-} else if (args.length === 1) {
-  console.log('1');
 } else {
   let largest = args[0];
   let nextLargest = 0;
-  for (let j = 0; j < args.length; j++) {
+  for (let j = 1; j < args.length; j++) {
     if (args[j] > largest) {
       nextLargest = largest;
       largest = args[j];
