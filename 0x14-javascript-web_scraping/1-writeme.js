0f24f8065e521b992df41fb451f43745b56e7076
feliciahsieh <felicia@tixwriteoff.com>
2018-01-10 18:42:17 +0000

    first commit with code

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+let args = process.argv.slice(2);
+if (args[0] === undefined) {
+  console.log('No argument');
+} else if (args.length !== 2) {
+  process.exit();
+}
+
+args[1] += '\n';
+let fs = require('fs');
+fs.writeFile(args[0], args[1], 'utf8', function (err) {
+  if (err) {
+    console.log(err);
+  }
+});
