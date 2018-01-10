9660ad59ff80b03e01c16c4dc311e32e36b26514
feliciahsieh <felicia@tixwriteoff.com>
2018-01-10 18:16:34 +0000

    first commit with code

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+let args = process.argv.slice(2);
+if (args[0] === undefined) {
+  console.log('No argument');
+} else if (args.length !== 1) {
+  process.exit();
+}
+
+let fs = require('fs');
+fs.readFile(args[0], 'utf8', function read (err, data) {
+  if (err) {
+    throw err;
+  }
+  console.log(data);
+});
