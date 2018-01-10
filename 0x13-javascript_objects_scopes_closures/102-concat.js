1b10d75470048a7e742ff82d9beb85a10148ae88
feliciahsieh <felicia@tixwriteoff.com>
2018-01-10 01:58:43 +0000

    add argv arguments and graceful exit if args != 3

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,16 +1,27 @@
 #!/usr/bin/node
 
+let args = process.argv.slice(2);
+if (args[0] === undefined) {
+  console.log('No argument');
+} else if (args.length === 3) {
+  f1 = args[0];
+  f2 = args[1];
+  f3 = args[2];
+} else {
+  process.exit();
+}
+
 let fs = require('fs');
 var content, content2;
 
-fs.readFile('./fileA', function read (err, data) {
+fs.readFile(f1, function read (err, data) {
   if (err) {
     throw err;
   }
   content = data;
 });
 
-fs.readFile('./fileB', function read (err, data2) {
+fs.readFile(f2, function read (err, data2) {
   if (err) {
     throw err;
   }
@@ -20,7 +31,7 @@ fs.readFile('./fileB', function read (err, data2) {
 
 function processFile () {
   let contentTotal = content + content2;
-  fs.writeFile('./fileC', contentTotal, 'utf8', function (err) {
+  fs.writeFile(f3, contentTotal, 'utf8', function (err) {
     if (err) {
       throw err;
     }
