cb68948c5a338100d0fa89731d8422c0f3b22055
feliciahsieh <felicia@tixwriteoff.com>
2018-01-10 08:20:08 +0000

    working version

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,18 +1,24 @@
 #!/usr/bin/node
 
 const { dict } = require('./101-data');
-console.log(dict);
-
 let a = {};
-let l = [];
-let i = 0;
-//object.keys(obj)
-for(key in dict) {
-  l = a[dict[key]];
-  console.log(l);
-  a[dict[key]] = key;
+let v = [];
+let temp = [];
+let key = 0;
+
+for (key in dict) {
+  temp = [];
+  v = dict[key];
+  if (a[v]) {
+    temp = [];
+    a[v].forEach(function (element) {
+      temp.push(element);
+    });
+    temp.push(key);
+    a[dict[key]] = temp;
+  } else {
+    temp.push(key);
+    a[v] = temp;
+  }
 }
-  
-
-
 console.log(a);
