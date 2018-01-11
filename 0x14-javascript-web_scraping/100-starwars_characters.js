3da8c46caa7cd77688bb1c59879b04d9d6c9acc0
feliciahsieh <felicia@tixwriteoff.com>
2018-01-11 00:01:03 +0000

    match episode to API query instead of using episode_id

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -8,7 +8,7 @@ if (args[0] === undefined) {
   process.exit();
 }
 
-let url = 'http://swapi.co/api/films/';
+let url = 'http://swapi.co/api/films/' + args[0];
 let request = require('request');
 request(url, function (error, response, body) {
   if (error) {
@@ -16,13 +16,11 @@ request(url, function (error, response, body) {
   }
   if (response.statusCode === 200) {
     let obj = JSON.parse(body);
-    let episodeId = +args[0];
-    let movieProfile = obj['results'][episodeId];
-    movieProfile['characters'].forEach(function (element2) {
+    let characters = obj['characters'];
+    characters.forEach(function (element2) {
       request(element2, function (error2, response2, body2) {
         if (error2) { // Handle error if one occurred
         }
-
         if (response2.statusCode === 200) {
 	  let obj2 = JSON.parse(body2);
 	  console.log(obj2['name']);
