dea774ab1da9ee311142cf7841c6c9cedbcfb860
feliciahsieh <felicia@tixwriteoff.com>
2018-01-11 00:42:13 +0000

    works but must install sync-request library

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+// Must install module, sync-request with
+// $npm install sync-request
+
+let args = process.argv.slice(2);
+if (args[0] === undefined) { // No user argument
+  process.exit();
+} else if (args.length !== 1) {
+  process.exit();
+}
+
+let base = 'http://swapi.co/api/films/' + args[0];
+
+var request = require('sync-request');
+
+try {
+  let res = request('GET', base);
+
+  let obj = res.getBody();
+  let obj2 = JSON.parse(obj);
+  let characters = obj2['characters'];
+
+  for (let i = 0; i < characters.length; i++) {
+    let resCharacter = request('GET', characters[i]);
+
+    let c = resCharacter.getBody();
+    let cJson = JSON.parse(c);
+    console.log(cJson['name']);
+  }
+} catch (e) {}
