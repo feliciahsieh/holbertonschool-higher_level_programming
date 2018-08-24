e5329761d7a28abafe8ee6e18af0bd415369a563
feliciahsieh <felicia@tixwriteoff.com>
2018-08-24 21:05:28 +0000

    fixing git problem after git reset head

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,17 +1,49 @@
 #!/usr/bin/python3
 class Node:
-    def __init__(self, n=0):
+    def __init__(self, data=0, next_node=None):
         __data = n
+        __next_node = None
+        if type(data) == int:
+            self.__data = data
+        else:
+            raise TypeError("data must be an integer")
+
+        if value == None or type(value) == type(Node):
+            self.__next_node = next_node
+            raise TypeError("next_node must be a Node object")
 
     @property
     def data(self):
-        """
-        Getter for data
-
-        Args:
-            self (Square): The square instance
-
-        Returns:
-            size of square
-        """
         return self.__data
+
+    @data.setter
+    def data(self, value):
+        if type(value) == int:
+            self.__data = value
+        else:
+            raise TypeError("data must be an integer")
+
+    @property
+    def next_node(self):
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        if value == None or type(value) == type(Node):
+            raise TypeError("next_node must be a Node object")
+        else:
+            self.__next_node = value
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.__head
+        self.next_node = None
+
+    def __str__(self):
+        curr = self.__head
+        while (curr != None):
+            print(curr.__head.data)
+            curr = curr->next_node
+
+    def sorted_insert(self, value):
+        
