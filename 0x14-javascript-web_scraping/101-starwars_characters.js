25c7a6d8c17d567e952cb65c632916bf3faf5e31
feliciahsieh <felicia@tixwriteoff.com>
2018-08-24 21:37:14 +0000

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -9,23 +9,41 @@ if (args[0] === undefined) { // No user argument
   process.exit();
 }
 
-let base = 'http://swapi.co/api/films/' + args[0];
+let base = 'http://swapi.co/api/films/' + args[0] + 'schema';
+const promiseSerial = funcs =>
+  funcs.reduce((promise, func) =>
+    promise.then(result => func().then(Array.prototype.concat.bind(result))),
+    Promise.resolve([]))
 
-var request = require('request');
-var request = require('sync-request');
-
-try {
-  let res = request('GET', base);
-
-  let obj = res.getBody();
-  let obj2 = JSON.parse(obj);
-  let characters = obj2['characters'];
-
-  for (let i = 0; i < characters.length; i++) {
-    let resCharacter = request('GET', characters[i]);
-
-    let c = resCharacter.getBody();
-    let cJson = JSON.parse(c);
-    console.log(cJson['name']);
+let request = require('request');
+let charList = [];
+request(url, function (error, response, body) {
+  if (error) {
+    // Handle error if one occurred
   }
-} catch (e) {}
+  if (response.statusCode === 200) {
+    let characters = body['characters'];
+    characters.forEach(function (element2) {
+      request(element2, function (error2, response2, body2) {
+        if (error2) { // Handle error if one occurred
+        }
+        if (response2.statusCode === 200) {
+          console.log(body2['name']);
+        }
+      });
+    });
+  }
+});
+
+
+
+
+// some url's to resolve
+const urls = ['/url1', '/url2', '/url3']
+
+// convert each url to a function that returns a promise
+const funcs = urls.map(url => () => $.ajax(url))
+
+// execute Promises in serial
+promiseSerial(funcs)
+  .then(console.log.bind(console))
