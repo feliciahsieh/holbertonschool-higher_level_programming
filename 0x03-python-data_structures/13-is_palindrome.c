3bfc794e5d2a965ee19ea427069c3b886c144793
feliciahsieh <felicia@tixwriteoff.com>
2018-08-26 19:41:16 +0000

    Removed extra statements. Refactored

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,22 +2,19 @@
 #include "lists.h"
 
 /**
- * is_palindrome - checks linked list if it's a palindrome
+ * is_palindrome - checks linked list if it is a palindrome
  * @head: pointer to head of linked list
  * Return: 0 if Not Palindrome
  *         1 if Palindrome (or empty list)
  */
 int is_palindrome(listint_t **head)
 {
-	const listint_t *current;
+	listint_t *current;
 	int word[1000], i = 0;
 	int left = 0, right = 0;
 
-	word[0] = word[0];
-
 	if (*head == NULL)
 		return (1);
-
 	current = *head;
 	while (current)
 	{
@@ -27,17 +24,11 @@ int is_palindrome(listint_t **head)
 	}
 	if (i == 1)
 		return (1);
-
+	left = i / 2 - 1;
 	if ((i % 2) == 0)
-	{
-		left = i / 2 - 1;
 		right = i / 2;
-	} else
-	{
-		left = i / 2 - 1;
+	else
 		right = i / 2 + 1;
-	}
-
 	while (left != 0)
 	{
 		if (word[left] != word[right])
@@ -45,6 +36,5 @@ int is_palindrome(listint_t **head)
 		left--;
 		right++;
 	}
-
 	return (1);
 }
