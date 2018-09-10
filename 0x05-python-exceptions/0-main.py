755fa82102eb4fa1bf07541d11bf5d830222af89
feliciahsieh <felicia@tixwriteoff.com>
2018-09-10 19:11:30 +0000

    changing main back to orig

--- 0x05-python-exceptions/0-main.py
+++ 0x05-python-exceptions/0-main.py
@@ -5,10 +5,6 @@ my_list = [1, 2, 3, 4, 5]
 
 nb_print = safe_print_list(my_list, 2)
 print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, -1)
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, 0)
-print("nb_print: {:d}".format(nb_print))
 nb_print = safe_print_list(my_list, len(my_list))
 print("nb_print: {:d}".format(nb_print))
 nb_print = safe_print_list(my_list, len(my_list) + 2)
