6b9dfa75a6fda8d65a0244e222407c7ec7532cd8
feliciahsieh <felicia@tixwriteoff.com>
2018-09-13 21:13:29 +0000

    refactoring

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,14 +3,11 @@ import sys
 
 
 def safe_print_integer_err(value):
-    result = False
     try:
         print("{:d}".format(value))
-    except ValueError as err:
-        sys.stderr.write("Exception: {}\n".format(str(err)))
-    except TypeError as err:
-        sys.stderr.write("Exception: {}\n".format(str(err)))
-    else:
         result = True
+    except (ValueError, TypeError) as e:
+        sys.stderr.write("Exception: {}\n".format(str(e)))
+        result = False
     finally:
         return result
