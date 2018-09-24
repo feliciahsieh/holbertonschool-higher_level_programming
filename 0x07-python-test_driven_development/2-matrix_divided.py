2f98c60043eb1669e4f426e7a619df0db3fd212a
feliciahsieh <felicia@tixwriteoff.com>
2018-09-24 20:11:40 +0000

    fix when matrix is None

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -18,11 +18,15 @@ def matrix_divided(matrix, div):
         return []
     if matrix is not None:
         nCol = len(matrix[0])
+    else:
+        raise TypeError("matrix must be a matrix (list of lists)\
+ of integers/floats")
+
     for i in matrix:
         if len(matrix[index]) != nCol:
             raise TypeError("Each row of the matrix must have the same size")
         for j in i:
-            if type(j) is not int and type(j) is not float:
+            if not(type(j) is int or type(j) not float):
                 raise TypeError("matrix must be a matrix (list of lists)\
  of integers/floats")
         index += 1
