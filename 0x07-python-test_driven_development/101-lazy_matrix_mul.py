800caf69a2bdca779fbf35281b4326caad2ca090
feliciahsieh <felicia@tixwriteoff.com>
2018-09-25 19:49:27 +0000

    pep8

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -14,7 +14,7 @@ def lazy_matrix_mul(m_a, m_b):
     """
 
     return np.matmul(m_a, m_b)
-    
+
 if __name__ == "__main__":
     import doctest
     doctest.testfile("tests/101-lazy_matrix_mul.txt")
