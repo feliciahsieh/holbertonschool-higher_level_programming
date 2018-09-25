fd557104f4f62ae13604dbfacf163b34437267e7
feliciahsieh <felicia@tixwriteoff.com>
2018-09-25 19:01:00 +0000

    reduced code

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -2,7 +2,6 @@
 """
     lazy_matrix_mul: multiply 2 matrices
 """
-
 import numpy as np
 
 
@@ -13,10 +12,6 @@ def lazy_matrix_mul(m_a, m_b):
     m_a (list): first matrix
     m_b (list): second matrix
     """
-
-    # STILL NEED TO ADD 100-xxx ERROR MSGS
-    if m_a == [] or m_b == []:
-        raise ValueError
 
     return np.matmul(m_a, m_b)
 
