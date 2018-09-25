e55d4cf5404e91fa02908eef176256cd88dfbd1d
feliciahsieh <felicia@tixwriteoff.com>
2018-09-25 21:53:00 +0000

    removing test comments

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -5,9 +5,6 @@
 
 
 def matrix_mul(m_a, m_b):
-    print("m_a:{}".format(m_a))
-    print("m_b:{}".format(m_b))
-
     if type(m_a) is not list:
         raise TypeError("m_a must be a list")
     if type(m_b) is not list:
