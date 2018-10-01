4f6eb059bc048a4b4a364e6e793306566e0b830a
feliciahsieh <felicia@tixwriteoff.com>
2018-10-01 22:15:07 +0000

    first commit. not working

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    101-nqueens.py - Place N non-attacking queens on an N×N chessboard
+    Calc N queens problem
+    Args: n - number of queens on an NxN chessboard
+"""
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    exit(1)
+n = int(sys.argv[1])
+if type(n) is not int:
+    print("N must be a number")
+    exit(1)
+if n < 4:
+    print("N must be at least 4")
+    exit(1)
+result = []
+for r in range(n):
+    for c in range(n):
+        # result.append([r, c])
+
+print(result)
