ea1a360d991332c4ad976c7eaaadbfa4edee2dfd
feliciahsieh <felicia@tixwriteoff.com>
2018-10-24 02:38:01 +0000

    remove print()

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -20,10 +20,10 @@ class Rectangle(BaseGeometry):
             None
         Return: None
         """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
+        if self.integer_validator("width", width) and width >= 0:
+            self.__width = width
+        if self.integer_validator("height", height) and height >= 0:
+            self.__height = height
 
     def area(self):
         """
@@ -35,15 +35,6 @@ class Rectangle(BaseGeometry):
         Return: area of Rectangle
         """
         return self.__width * self.__height
-
-    def print(self):
-        """
-        print - print the Rectangle area
-        Arg:
-            None
-        Return: area of Rectangle
-        """
-        return self.area
 
     def __str__(self):
         """
