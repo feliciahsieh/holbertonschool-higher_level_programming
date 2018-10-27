340fdaa30961c337425484cb4a9ad505fa959aa5
feliciahsieh <felicia@tixwriteoff.com>
2018-10-27 04:19:43 +0000

    refactoring

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -11,10 +11,7 @@ def save_to_json_file(my_obj, filename):
     save_to_json_file - writes object to text file using a JSON representation
     """
 
-    with open(filename, 'w+') as f:
-        pass
-
-    if type(filename) is not str:
+    if type(filename) is not str or filename == "":
         return 0
     jsonStr = json.dumps(my_obj)
 
