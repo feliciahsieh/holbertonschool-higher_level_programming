395235822680538a0aa1f3e58240e47437aa948f
feliciahsieh <felicia@tixwriteoff.com>
2018-11-19 01:41:43 +0000

    Use the Rectangle object name in __str__()

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -118,8 +118,10 @@ class Rectangle(Base):
         Return:
             None
         """
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(
-            self.id, self.x, self.y, self.width, self.height)
+        return "[{}] ({}) {}/{} - {}/{}".format(
+            self.__class__.__name__, self.id,
+            self.x, self.y,
+            self.width, self.height)
 
     def update(self, *args, **kwargs):
         """
