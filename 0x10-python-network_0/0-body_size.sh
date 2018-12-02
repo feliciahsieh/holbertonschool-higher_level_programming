3bfb648b9c01d739536975c08dc98cded9563fa8
feliciahsieh <felicia@tixwriteoff.com>
2018-12-02 18:12:53 +0000

    refactored

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a request to a URL and display size of the response body
-curl -sw "%{size_download}" $1 -o /dev/null | grep -oE '[0-9]+$' | tail -1
+curl -sw "%{size_download}" $1 -o /dev/null ; echo
