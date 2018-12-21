5d0ba13e9aed3ff37bfbac0920c6b34601ac4621
feliciahsieh <felicia@tixwriteoff.com>
2018-12-21 01:35:23 +0000

    removed doublequote in filename

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send POST request to URL with data in file and show body of the response
-curl -H "Content-Type: application/json" --request POST --data @"$2" "$1"
+curl -H "Content-Type: application/json" --request POST --data @$2 "$1"
