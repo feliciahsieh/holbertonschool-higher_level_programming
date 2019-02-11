4cc7a2013010ed1ff71566ab32ce5db62da85254
feliciahsieh <felicia@tixwriteoff.com>
2019-02-11 18:57:41 +0000

    first commit with code. Not finished

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,34 @@
 #!/usr/bin/python3
-""" 4-hbtn_status.py """
-if __name__ == "__main__":
-    """ __main__ """
-    import requests
+""" 4-json_api.py - send user input in a POST request & receives JSON data """
 
-    url = 'https://intranet.hbtn.io/status'
-    r = requests.get(url)
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+if __name__ == "__main__":
+
+    import requests
+    import sys
+
+    url = "http://0.0.0.0:5000/search_user"
+    params = {}
+
+    if len(sys.argv) == 1:
+        params['q'] = ''
+    elif len(sys.argv) == 2:
+        params = {'q': sys.argv[1]}
+    else:
+        print("Usage: [URL] | [URL] [POST value]")
+        sys.exit(0)
+
+    # print("my params: {}".format(params))
+
+    r = requests.post(url, data=params)
+
+    try:
+        resp = r.json()
+        # print("my status code: {}".format(r.status_code))
+        if r.status_code == 200:  # Valid JSON
+            print("[{}] {}".format(resp['id'], resp['name']))
+        elif r.status_code == 204:  # Empty JSON
+            print("No result")
+        else:  # Invalid JSON
+            print("Not a valid JSON")
+    except Exception as e:
+        print("Not a valid JSON")
